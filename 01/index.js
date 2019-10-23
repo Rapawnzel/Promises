69ac49542f77ce9dd5edf6495088107636abec4b
n i n a <anndree.tg@gmail.com>
2019-10-24 00:30:13 +0200

    init

---
+++ 01/index.js
@@ -0,0 +1,6 @@
+let now = new Date();
+let day = now.getDate();
+let month = now.getMonth();
+let year = now.getFullYear();
+
+console.log(day"-"month"-"year);
---
+++ 02/index.js
@@ -0,0 +1,4 @@
+function getDaysinMonth(month, year);
+    new date = new Date(`${year}-${month}`);
+    for (days in )
+    console.log(Date.get)
